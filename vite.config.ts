71d7e2343162d99ebc38b6a048eee9a50e563e5c
Fouad Kh <fouad001kh@gmail.com>
2025-10-24 12:12:26 +0300

    Update to deploy in gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/internshipTracker/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+  server: {
+    open: true,
+    port: 3001,
+  },
+  }
+);
